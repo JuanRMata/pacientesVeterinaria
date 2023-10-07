fa99d4e0bcd0e7b763d54fadad80e3945ed22547
JuanRMata <jmata2@ucol.mx>
2023-10-07 11:15:55 -0600

    Implementacion de local storage mas npm run build para la carpeta dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
@@ -7,6 +7,19 @@ import ListadoPacientes from "./components/ListadoPacientes"
 function App() {
   const [pacientes,setPacientes] = useState([]) //Aqui vamos a llenar los datos con los del useState que tenemos en Formulario
   const [paciente, setPaciente] = useState({})
+
+  useEffect(() => { //Este useEffect va a obtener lo que haya en el localstorage
+    const obtenerLS = () =>{
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [] // con dos ?? quiere decir, SINO, con ? quiere decir SI
+      //JSON.parse sirve para convertir un string a un arreglo
+      setPacientes(pacientesLS)
+    }
+    obtenerLS() 
+  },[]) // NOTA: SI NO LE PASAS NADA DENTRO DE [] QUIERE DECIR QUE SOLO SE VA A EJECUTAR UNA VEZ
+
+  useEffect(() => { // Creando este useEffect para poder hacer la creacion del local storage
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  },[pacientes])
 
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
